5ae665094ebecc08687fd17dc3ed904c52f6f37a
Michal Trojanowski <mtroja98@gmail.com>
2025-07-23 14:53:44 +0200

    DTO model used for test

--- src/main/java/com/michal/github/controller/RepositoriesRestController.java
+++ src/main/java/com/michal/github/controller/RepositoriesRestController.java
@@ -2,12 +2,17 @@ package com.michal.github.controller;
 
 import java.io.IOException;
 
-import org.apache.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.michal.github.entity.StatusResponse;
+import com.michal.github.exception.UserNotFoundException;
 import com.michal.github.service.GithubService;
 
 @RequestMapping("/v1")
@@ -17,10 +22,17 @@ public class RepositoriesRestController {
 	@Autowired
 	private GithubService githubService;
 	
-	@RequestMapping("/repositories")
-	public String doGet(@RequestParam String login) throws ParseException, IOException
-	{
-		return githubService.getUserReposWithBranches(login);
+	@GetMapping("/repos")
+	public ResponseEntity<?> getUserRepos(@RequestParam String login) throws IOException {
+	    try {
+	        String json = githubService.getUserReposWithBranches(login);
+	        return ResponseEntity.ok()
+	                .contentType(MediaType.APPLICATION_JSON)
+	                .body(json);
+	    } catch (UserNotFoundException ex) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+	                .body(new StatusResponse(404, ex.getMessage()));
+	    }
 	}
 
 }
